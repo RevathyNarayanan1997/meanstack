b1ac9700c9f294ec7595bd3d29d65c91d3eec033
Revathy <revathynarayanan1997@gmail.com>
2021-06-10 11:50:53 +0530

    adding files

---
+++ javascript/fundamentalindex.js
@@ -0,0 +1,13 @@
+
+//  var state="kerala";
+// var active_cases=1500;
+// var cured=20000;
+ //   var death=200;
+// console.log(`in ${state} ${active_cases} new cases and ${cured} death ${death} has been reported`);
+
+var company="luminar";
+var rating=4.8;
+
+
+console.log(typeof(comapny));
+console
---
+++ javascript/swapping.js
@@ -0,0 +1,8 @@
+var num1=10,num2=20;
+console.log(`values b4 swapping num1=${num1} num2=${num2}`); //10,20
+
+var temp=num1;//temp=10
+num1=num2;//num1=20
+num2=temp;//num2=10
+
+console.log(`values after swapping num1=${num1} num2=${num2}`); //20,10
